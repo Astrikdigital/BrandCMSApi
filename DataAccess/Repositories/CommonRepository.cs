738c185a9eb3f4747670b1cd0fbe4a59f7f4b331
DevNabeelkhan <nk5173948@gmail.com>
2025-02-20 05:24:21 +0500

    new changes

--- DataAccess/Repositories/CommonRepository.cs
+++ DataAccess/Repositories/CommonRepository.cs
@@ -24,8 +24,6 @@ namespace DataAccessLayer.Repositories
             _httpContextAccessor = httpContextAccessor;
         }
 
-        
-        public async Task<dynamic> GetPageByType(string Slug)
         public async Task<dynamic> GetSchools()
         {
             try
@@ -46,122 +44,6 @@ namespace DataAccessLayer.Repositories
             {
                 var param = new
                 {
-                    Slug = Slug
-                };
-                using var connection = _context.CreateConnection();
-                var listschoolpage = (await connection.QueryAsync<SchoolModel>("GetPageByType", param: param)).ToList();
-                dynamic obj = new ExpandoObject();
-                var objDict = (IDictionary<string, object>)obj;
-                foreach (var item in listschoolpage)
-                    if (item.Value != null)
-                    {
-                        var programContainer = JsonSerializer.Deserialize<Dictionary<string, object>>(item.Value);
-                        if (programContainer.ContainsKey("JSON"))
-                        {
-                            var json = JsonSerializer.Deserialize<dynamic>(programContainer["JSON"].ToString());
-
-                            programContainer["JSON"] = json;
-                        }
-                        objDict[item?.Key?.Replace(" ", string.Empty)] = programContainer;
-                    }
-                return objDict;
-            }
-            catch (Exception ex)
-            {
-                return null;
-            }
-        }
-        public async Task<dynamic> InsertupdateAboutTheCourse(AboutTheCourseModel AboutTheCourse)
-        {
-            var resp = new Object();
-            try
-            { 
-                using var connection = _context.CreateConnection();
-                resp = await connection.QueryFirstOrDefaultAsync<dynamic>("InsertupdateAboutTheCourse", AboutTheCourse);
-                return resp;
-
-            }
-            catch (Exception ex)
-            {
-                return null;
-            } 
-        }
-        public async Task<dynamic> GetMajor(int? Id)
-        {
-            var resp = new Object();
-            try
-            {
-                using var connection = _context.CreateConnection();
-                resp = (await connection.QueryAsync<dynamic>("GetMajor", new { Id = Id })).ToList();
-                return resp;
-
-            }
-            catch (Exception ex)
-            {
-                return null;
-            }
-        }
-        public async Task<dynamic> GetMajorProgram(int? Id)
-        {
-            var resp = new Object();
-            try
-            {
-                using var connection = _context.CreateConnection();
-                resp = (await connection.QueryAsync<dynamic>("GetMajorProgram", new { Id = Id })).ToList();
-                return resp;
-
-            }
-            catch (Exception ex)
-            {
-                return null;
-            }
-        }
-        public async Task<dynamic> GetPotentialJobfield(int? Id)
-        {
-            var resp = new Object();
-            try
-            {
-                using var connection = _context.CreateConnection();
-                resp = (await connection.QueryAsync<dynamic>("GetPotentialJobfield", new { Id = Id })).ToList();
-                return resp;
-
-            }
-            catch (Exception ex)
-            {
-                return null;
-            }
-        }
-        public async Task<dynamic> GetBenefit(int? Id)
-        {
-            var resp = new Object();
-            try
-            {
-                using var connection = _context.CreateConnection();
-                resp = (await connection.QueryAsync<dynamic>("GetBenefit", new { Id = Id })).ToList();
-                return resp;
-
-            }
-            catch (Exception ex)
-            {
-                return null;
-            }
-        }
-        public async Task<dynamic> GetStudentSuccess(int? Id)
-        {
-            var resp = new Object();
-            try
-            {
-                using var connection = _context.CreateConnection();
-                resp = (await connection.QueryAsync<dynamic>("GetStudentSuccess", new { Id = Id })).ToList();
-                return resp;
-
-            }
-            catch (Exception ex)
-            {
-                return null;
-            }
-        }
-        
                     Id = Id
 
                 };
@@ -420,5 +302,127 @@ namespace DataAccessLayer.Repositories
             }
 
         }
+               public async Task<dynamic> GetPageByType(string Slug)
+        {
+            try
+            {
+                var param = new
+                {
+                    Slug = Slug
+                };
+                using var connection = _context.CreateConnection();
+                var listschoolpage = (await connection.QueryAsync<SchoolModel>("GetPageByType", param: param)).ToList();
+                dynamic obj = new ExpandoObject();
+                var objDict = (IDictionary<string, object>)obj;
+                foreach (var item in listschoolpage)
+                    if (item.Value != null)
+                    {
+                        var programContainer = JsonSerializer.Deserialize<Dictionary<string, object>>(item.Value);
+                        if (programContainer.ContainsKey("JSON"))
+                        {
+                            var json = JsonSerializer.Deserialize<dynamic>(programContainer["JSON"].ToString());
+
+                            programContainer["JSON"] = json;
+                        }
+                        objDict[item?.Key?.Replace(" ", string.Empty)] = programContainer;
+                    }
+                return objDict;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+        public async Task<dynamic> InsertupdateAboutTheCourse(AboutTheCourseModel AboutTheCourse)
+        {
+            var resp = new Object();
+            try
+            { 
+                using var connection = _context.CreateConnection();
+                resp = await connection.QueryFirstOrDefaultAsync<dynamic>("InsertupdateAboutTheCourse", AboutTheCourse);
+                return resp;
+
+            }
+            catch (Exception ex)
+            {
+                return null;
+            } 
+        }
+        public async Task<dynamic> GetMajor(int? Id)
+        {
+            var resp = new Object();
+            try
+            {
+                using var connection = _context.CreateConnection();
+                resp = (await connection.QueryAsync<dynamic>("GetMajor", new { Id = Id })).ToList();
+                return resp;
+
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+        public async Task<dynamic> GetMajorProgram(int? Id)
+        {
+            var resp = new Object();
+            try
+            {
+                using var connection = _context.CreateConnection();
+                resp = (await connection.QueryAsync<dynamic>("GetMajorProgram", new { Id = Id })).ToList();
+                return resp;
+
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+        public async Task<dynamic> GetPotentialJobfield(int? Id)
+        {
+            var resp = new Object();
+            try
+            {
+                using var connection = _context.CreateConnection();
+                resp = (await connection.QueryAsync<dynamic>("GetPotentialJobfield", new { Id = Id })).ToList();
+                return resp;
+
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+        public async Task<dynamic> GetBenefit(int? Id)
+        {
+            var resp = new Object();
+            try
+            {
+                using var connection = _context.CreateConnection();
+                resp = (await connection.QueryAsync<dynamic>("GetBenefit", new { Id = Id })).ToList();
+                return resp;
+
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+        public async Task<dynamic> GetStudentSuccess(int? Id)
+        {
+            var resp = new Object();
+            try
+            {
+                using var connection = _context.CreateConnection();
+                resp = (await connection.QueryAsync<dynamic>("GetStudentSuccess", new { Id = Id })).ToList();
+                return resp;
+
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+        
     }
 }
