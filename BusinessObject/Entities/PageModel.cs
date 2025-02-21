e8b1bda602b348e16ad22072176f1eb19a99b3c0
DevNabeelkhan <nk5173948@gmail.com>
2025-02-22 02:02:57 +0500

    add

--- BusinessObject/Entities/PageModel.cs
+++ BusinessObject/Entities/PageModel.cs
@@ -9,18 +9,18 @@ namespace BusinessObjectsLayer.Entities
     public class PageModel
     {
         public int? Id { get; set; }
-        public string SectionIds { get; set; }
-        public string Page { get; set; }
-        public string About { get; set; }
-        public string Admissions { get; set; }
-        public string Banner { get; set; }
-        public string ContactUS { get; set; }
-        public string FAQ { get; set; }
-        public string ProgramOffered { get; set; }
-        public string StudentSuccess { get; set; }
-        public string Testimonial { get; set; }
-        public string UnlockaWorld { get; set; }
-        public string WhyChooseus { get; set; }
+        public string? SectionIds { get; set; }
+        public string? Page { get; set; }
+        public string? About { get; set; }
+        public string? Admissions { get; set; }
+        public string? Banner { get; set; }
+        public string? ContactUS { get; set; }
+        public string? FAQ { get; set; }
+        public string? ProgramOffered { get; set; }
+        public string? StudentSuccess { get; set; }
+        public string? Testimonial { get; set; }
+        public string? UnlockaWorld { get; set; }
+        public string? WhyChooseus { get; set; }
 
     }
      
